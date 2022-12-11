af596bdfea32c5686c76b70b561c654f78c1d171
nikolas-poberezhniy <nikolas.poberezhniy@gmail.com>
2022-12-11 18:01:36 +0200

    finish

---
+++ src/components/phoneBook/ContactsList/ContactList.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { ListItem } from '@mui/material';
+
+export const Item = styled(ListItem)`
+  :hover {
+    background-color: #4399de;
+    border-radius: 8px;
+  }
+`;
